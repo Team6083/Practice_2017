d01429788eadd8cd6de60d85f0de68d1461a2a3f
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2016-12-08 13:02:51 +0800

    fix bug add z control

--- src/Systems/DriveBase.java
+++ src/Systems/DriveBase.java
@@ -36,8 +36,8 @@ public class DriveBase {
     		speedl=(-joy.getRawAxis(1)-joy.getRawAxis(0)+joy.getRawAxis(2))/speed_down_value;//left wheel's speed
     		speedr=(-joy.getRawAxis(1)+joy.getRawAxis(0)-joy.getRawAxis(2))/speed_down_value;//right wheel's speed
     	} else {
-        	speedl=(-joy.getRawAxis(1)+joy.getRawAxis(0))/speed_down_value;//left wheel's speed
-        	speedr=(-joy.getRawAxis(1)-joy.getRawAxis(0))/speed_down_value;//right wheel's speed
+        	speedl=(-joy.getRawAxis(1)+joy.getRawAxis(0)+joy.getRawAxis(2))/speed_down_value;//left wheel's speed
+        	speedr=(-joy.getRawAxis(1)-joy.getRawAxis(0)-joy.getRawAxis(2))/speed_down_value;//right wheel's speed
     	}
     	if(joy.getRawAxis(1)  == 0){
     		if(speedl >= 0.05 ){
