6a59866887411d8c39b37ba7579380162d24b57b
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2016-12-09 00:42:02 +0800

    create drive control mode switch

--- src/Systems/DriveBase.java
+++ src/Systems/DriveBase.java
@@ -2,6 +2,7 @@ package Systems;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveBase {
@@ -16,11 +17,20 @@ public class DriveBase {
     static double speedl = 0, speedr = 0, speed_down_value = 4.0;
     static boolean speedup = false;
     
+    final static String joy_xbox = "Xbox";
+    final static String joy_3dpro = "3D Pro";
+    static String joy_mode_Selected;
+    static SendableChooser joy_mode;
+    
     public static void init(){
     	motor1 = new VictorSP(motor1_channel);
     	motor2 = new VictorSP(motor2_channel);
     	joy= new Joystick(Joystick_port);
     	SmartDashboard.putNumber("speed_down_value", speed_down_value);
+    	joy_mode = new SendableChooser();
+    	joy_mode.addDefault("3D Pro", joy_3dpro);
+    	joy_mode.addObject("Xbox", joy_xbox);
+        SmartDashboard.putData("joy_control_mode", joy_mode);
     }
     
     public static void dashboard(){
@@ -28,17 +38,30 @@ public class DriveBase {
 		SmartDashboard.putNumber("right", motor2.get());
 		speed_down_value = SmartDashboard.getNumber("speed_down_value");
 		SmartDashboard.putBoolean("speedup", speedup);
+		joy_mode_Selected = (String) joy_mode.getSelected();
+		
     }
     
     public static void teleOp(){
     	dashboard();
-    	if(joy.getRawAxis(1) < 0){
-    		speedl=(-joy.getRawAxis(1)-joy.getRawAxis(0)-joy.getRawAxis(2))/speed_down_value;//left wheel's speed
-    		speedr=(-joy.getRawAxis(1)+joy.getRawAxis(0)+joy.getRawAxis(2))/speed_down_value;//right wheel's speed
-    	} else {
-        	speedl=(-joy.getRawAxis(1)+joy.getRawAxis(0)-joy.getRawAxis(2))/speed_down_value;//left wheel's speed
-        	speedr=(-joy.getRawAxis(1)-joy.getRawAxis(0)+joy.getRawAxis(2))/speed_down_value;//right wheel's speed
+    	switch(joy_mode_Selected) {
+    		case joy_xbox:
+            //Control with XBox remote 
+    			
+                break;
+        	case joy_3dpro:
+        	default:
+        	//Control with 3DPro remote
+        		if(joy.getRawAxis(1) < 0){
+            		speedl=(-joy.getRawAxis(1)-joy.getRawAxis(0)-joy.getRawAxis(2))/speed_down_value;//left wheel's speed
+            		speedr=(-joy.getRawAxis(1)+joy.getRawAxis(0)+joy.getRawAxis(2))/speed_down_value;//right wheel's speed
+            	} else {
+                	speedl=(-joy.getRawAxis(1)+joy.getRawAxis(0)-joy.getRawAxis(2))/speed_down_value;//left wheel's speed
+                	speedr=(-joy.getRawAxis(1)-joy.getRawAxis(0)+joy.getRawAxis(2))/speed_down_value;//right wheel's speed
+            	}
+                break;
     	}
+    	
     	if(joy.getRawAxis(1)  == 0){
     		if(speedl >= 0.05 ){
     			speedl=speedl-0.05;	
